6ec964dfc3a1349d60ebaefce58cdd157fe25f39
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2025-11-29 22:35:32 +0300

    InMemoryCache

---
+++ InMemoryCaching/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("set/{name}")]
+        public void Set(string name)
+        {
+            _memoryCache.Set("name", name,options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(3)
+            });
+        }
+
+
+
+        [HttpGet]
+        public string Get()
+        {
+            _memoryCache.TryGetValue<string>("name", out string value);
+            return value;
+        }
+    }
+}
